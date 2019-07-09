bf86b3d431231ee629dab3dd80fb2aec5a5fe49d
kmilo93sd <kmilo93sd@gmail.com>
2019-07-09 17:37:27 -0400

    fixed

--- database/scrapp.sql
+++ database/scrapp.sql
@@ -8,20 +8,3 @@ CREATE TABLE "users" (
 ) WITH (
   OIDS=FALSE
 );
-
-CREATE TABLE "projects" (
-	"id" varchar(36) NOT NULL,
-	"title" varchar(30) NOT NULL,
-	"created_at" TIMESTAMP NOT NULL,
-	"updated_at" TIMESTAMP NOT NULL,
-	"user_id" varchar(36) NOT NULL,
-	CONSTRAINT projects_pk PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
-);
-
-
-
-
-ALTER TABLE "projects" ADD CONSTRAINT "projects_fk0" FOREIGN KEY ("user_id") REFERENCES "users"("id");
-
